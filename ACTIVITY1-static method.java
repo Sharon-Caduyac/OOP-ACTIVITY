7b5419ab039925efbe174866bf6b7d9707feae41
Sharon-Caduyac <148937572+Sharon-Caduyac@users.noreply.github.com>
2023-10-25 16:22:01 +0800

    Update ACTIVITY1-static method.java

---
+++ ACTIVITY1-static method.java
@@ -0,0 +1,24 @@
+public class Average {
+
+  public static void main(String[] args) {
+        double[] numbers = { 5.5, 10.0, 15.5, 20.0, 25.5 };
+        double average = calculateAverage(numbers);
+
+        System.out.println("Average: " + average);
+    }
+    
+    public static double calculateAverage(double[] numbers) {
+        if (numbers.length == 0) {
+            return 0.0;
+        }
+
+        double sum = 0;
+        for (double number : numbers) {
+            sum += number;
+        }
+
+        return sum / numbers.length;
+    }
+
+  
+}
